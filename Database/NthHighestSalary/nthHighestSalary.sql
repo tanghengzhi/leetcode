5db93727e4d252bd2e5df0346d1de98c806d9587
Victor Tang <tanghengzhi@live.com>
2025-01-04 21:51:29 +0800

    Delete Database/NthHighestSalary directory

--- Database/NthHighestSalary/nthHighestSalary.sql
+++
@@ -1,9 +0,0 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-DECLARE M INT;
-SET M=N-1;
-  RETURN (
-      # Write your MySQL query statement below.
-      select IFNULL((select distinct Salary from Employee order by Salary desc limit M, 1), null)
-  );
-END
